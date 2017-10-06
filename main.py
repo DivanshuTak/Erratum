56b5f951a05da84db522f0a75728f6ad0ce8189b
DivanshuTak <divanshutak66@gmail.com>
2017-10-07 00:01:28 +0530

    improvements

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ file_name = raw_input("Enter csv file path \n")
 with open(file_name , "rb") as f:
     reader = csv.reader(f)
     for row in reader:
-        myfirstscript.fullpage_screenshot_chrome(row[0], "/home/divanshu/new/"+row[1]+".png" )
+    	#add file name where you want to save image
+        screenshot.fullpage_screenshot_chrome(row[0], "/home/divanshu/new/"+row[1]+".png" )
 
